64541186b5398c23eb98f3d81a768e3e151c53c4
awol2005ex <awol2005ex@163.com>
2024-12-04 11:41:48 +0800

    增加结构显示

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,8 @@ async function read_orc_file(filename:string) {
 
   columns.value = result.columns;
   data.value = result.rows;
+
+  orc_struct.value= JSON.stringify(result.columns);
 }
 
 const data = ref([]);
@@ -38,25 +40,46 @@ interface Column {
   name: string
 }
 const columns :Ref<Array<Column>> = ref([]);
+
+const strunctdrawer = ref(false);
+
+const orc_struct = ref("");
+
+const openStruct = async () => {
+  strunctdrawer.value =  !strunctdrawer.value;
+}
+
 </script>
 
 <template>
   <el-form :model="orcFileForm" label-width="150px" size="small" @submit.native.prevent>
     <el-form-item label="Input File Path:">
       <input  
-        style="width: 90%"
+        style="width: 300px"
         clearable
         v-model="orcFileForm.inputFiles"
       />
       <el-button @click="openFile">Open</el-button>
+      <el-button @click="openStruct">show struct</el-button>
     </el-form-item>
   </el-form>
 
   <br />
   <el-table :data="data" min-height="240" border fit>
     <el-table-column label="__rowindex" width="150px" type="index" />
-    <el-table-column v-for="column in columns" :key="column.name" :prop="column.name" :label="column.name" min-width="100px"/>
+    <el-table-column v-for="column in columns" :key="column.name" :prop="column.name" :label="column.name" min-width="100px">
+      <template #header>
+       <span :title="column.data_type"> {{ column.name }}</span>
+      </template>
+    </el-table-column>
   </el-table>
+
+  <el-drawer
+    v-model="strunctdrawer"
+    title="Struct"
+  >
+    {{ orc_struct }}
+  </el-drawer>
 </template>
 
 <style scoped></style>
